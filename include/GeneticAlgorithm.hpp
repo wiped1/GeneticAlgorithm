ee495e43e293a395de8607ad9c898b905e9e5171
Mateusz Dudek <dudek.mateusz92@gmail.com>
2015-03-01 13:06:02 +0100

    Added single include file containing includes to all library headers

---
+++ include/GeneticAlgorithm.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Genotype.hpp"
+#include "GenotypeInitializer.hpp"
+#include "Population.hpp"
+#include "PopulationInitializer.hpp"
+#include "Evaluator.hpp"
+#include "CrossoverStrategy.hpp"
+#include "MutationStrategy.hpp"
