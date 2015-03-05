ff27cac3a243c6d8a28d21835524fc1ed4bf4496
Mateusz Dudek <dudek.mateusz92@gmail.com>
2015-03-05 17:55:33 +0100

    GenotypeInitializer initialize member function is now const

--- include/GenotypeInitializer.hpp
+++ include/GenotypeInitializer.hpp
@@ -6,5 +6,5 @@ template <typename T>
 class GenotypeInitializer {
 public:
     virtual ~GenotypeInitializer() = default;
-    virtual void initialize(std::vector<T> &genes) = 0;
+    virtual void initialize(std::vector<T> &genes) const = 0;
 };
